bdc97f8c036006a2bdafd884ede22e289ca8095d
EnriqueMendozaPrrfect <lionsixeight@gmail.com>
2023-06-05 23:48:30 -0600

    add tutorial 1 and 2

---
+++ 1-hello-world/helloWorld.js
@@ -0,0 +1,16 @@
+console.log('Hello world');
+
+const x = 'hello world';
+console.log(x);
+
+let y = 5;
+console.log(y);
+
+y = 6;
+console.log(y);
+
+y = 'abc';
+console.log(y);
+
+isValid = false;
+console.log(isValid);
---
+++ 2-aritmetica/aritmetica.js
@@ -0,0 +1,25 @@
+// () Parentesis
+// / y * División y multiplicación
+// + y -	Suma y resta
+// La forma en la que se resuelven es de izquierda a derecha
+
+let suma = 5 + 6;
+console.log('El valor de la suma es', suma);
+
+let resta = 5 - 2;
+console.log('El valor de la resta es', resta);
+
+let multiplicación = 3 * 4;
+console.log('El valor de la multiplicación es', multiplicación);
+
+let division = 12 % 3;
+console.log('El valor de la division es', division);
+
+let parentesis = (5 + 3) * 2;
+console.log('El valor de la parentesis es', parentesis);
+
+// Ejercicios
+// Calcula el área de un cuadrado de 8 e imprime el resultado
+// Calcula el área de un triangulo de base 5 y altura 7 e imprime el resultado
+// Calcula el área de un trapesio de lado superior es de 5 y el lado inferior 7 
+// con altura de 4 e imprime el resultado.
